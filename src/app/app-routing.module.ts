ea46834d712ce8553dc02425f2a14cf4ddb405e9
Rocio <arncard@gmail.com>
2020-09-08 20:50:15 -0500

    create header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'checkout', component: },
+  { path: 'login' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
