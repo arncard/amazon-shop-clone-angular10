496954b5873525fc0590e6b9b56597f1fce516d0
Rocio <arncard@gmail.com>
2020-11-05 22:11:25 -0500

    login styles

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  enteredEmail: string = "";
+  continuing: boolean;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  continue($event)
+  {
+    // preventDefault() stops the refresh when clicking the button. 
+    // this happens because the button is inside a form tag.
+    $event.preventDefault();
+    this.continuing = true;
+  }
 }
